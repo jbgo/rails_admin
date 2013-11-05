4715b8edc2cd36335d15b115ea1065a7df3a44eb
Jordan Bach <jordanbach@gmail.com>
2013-11-05 13:38:05 -0600

    Show most recent versions first

--- lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
+++ lib/rails_admin/extensions/paper_trail/auditing_adapter.rb
@@ -45,7 +45,7 @@ module RailsAdmin
         end
 
         def latest
-          ::Version.limit(100).map{|version| VersionProxy.new(version, @user_class)}
+          ::Version.order('created_at desc').limit(100).map{|version| VersionProxy.new(version, @user_class)}
         end
 
         def delete_object(object, model, user)
